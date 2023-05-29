6b5700f6b273bff2ffd3e489c2cd0c6559cf2c2a
Lim_JiSeon <dpf226@naver.com>
2023-05-30 00:19:38 +0900

    [FEAT] 커뮤니티 글 작성 페이지 이벤트 처리 및 서버 연결 완료

--- src/containers/Writing.jsx
+++ src/containers/Writing.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Style = {
   Wrapper: styled.div`
@@ -34,7 +37,7 @@ const Style = {
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 5vh 0 1vh 0;
+    padding: 5vh 1vw 1vh 0;
   `,
   WriteName: styled.div`
     font-size: 18px;
@@ -42,7 +45,7 @@ const Style = {
     color: #878787;
     padding-right: 7vw;
   `,
-  TitleInput: styled.input`
+  TitleInput: styled.input.attrs({ required: true })`
     width: 50vw;
     height: 6vh;
     border: 2px solid #C3C3C3;
@@ -123,6 +126,20 @@ const Style = {
     border-radius: 20px;
     padding: 6vh 6vw;
     background-color: #FFFFFF;
+  `,
+  Image: styled.img`
+    width: 10vw;
+    height: 10vw;
+    border-radius: 20px;
+    margin: 1vh 2vw;
+  `,
+  WritingImg: styled.div`
+    display: flex;
+    align-items: start;
+  `,
+  ImgDelBtn: styled.button`
+    border: none;
+    background-color: transparent;
   `,
   WriteTagWrap: styled.div`
     display: flex;
@@ -205,9 +222,13 @@ const Style = {
     font-size: 24px;
     font-weight: bold;
   `,
+  Span: styled.span`
+    color: red;
+  `
 };
 
 function Writing() {
+  const navigate = useNavigate();
   const [input, setInput] = useState({
     "memberToken" : "",
     "title" : "",
@@ -215,8 +236,14 @@ function Writing() {
     "feedImageUrls" : [],
     "feedTags" : []
   });
-  const [tagTxt, setTagTxt] = useState("")
-  const [tagBlocks, setTagBlocks] = useState([])
+  const [tagTxt, setTagTxt] = useState("");
+  const [tagBlocks, setTagBlocks] = useState([]);
+  const [previewImage, setPreviewImage] = useState(["","",""]);
+  const [reqImage, setReqImage] = useState(["","",""]);
+
+  const img1Input = useRef();
+  const img2Input = useRef();
+  const img3Input = useRef();
 
   const { memberToken, title, body, feedImageUrls, feedTags } = input;
 
@@ -228,26 +255,120 @@ function Writing() {
     });
   };
 
+  const onChangeImg = (e, id) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        previewImage[id] = reader.result;
+        reqImage[id] = e.target.files[0];
+        setPreviewImage([...previewImage]);
+        setReqImage([...reqImage]);
+        resolve();
+      };
+    });
+  };
+
+  const deleteImg = (url) => {
+    for(let i=0; i<3; i++) {
+      if(previewImage[i] == url){
+        previewImage[i] = "";
+        reqImage[i] = "";
+        setPreviewImage([...previewImage]);
+        setReqImage([...reqImage]);
+      }
+    }
+  }
+
   const createTag = () => {
     feedTags.push(tagTxt);
     setInput({...input, ["feedTags"]:feedTags});
     let newTag = (
-      <Style.Tag key={tagBlocks.length} id={tagTxt}>
+      <Style.Tag key={tagBlocks.length}>
         <Style.DelBtn onClick={() => deleteTag(tagTxt)}><FontAwesomeIcon icon={faXmark} color="#FFFFFF" /></Style.DelBtn>
         <Style.Txt># {tagTxt}</Style.Txt>
       </Style.Tag>
     );
-    setTagBlocks(current => [...current, newTag]);
+    let element = {
+      id: tagTxt, 
+      value: newTag
+    };
+    tagBlocks.push(element);
+    setTagBlocks(tagBlocks);
   };
 
   const deleteTag = (id) => {
-    const tag = document.getElementById(id);
-    console.log(tag.innerText);
     setTagBlocks(oldValues => {
-      return oldValues.filter(tagBlock => console.log(tagBlock.innerText))// != tag.innerText)
+      return oldValues.filter(tagBlock => tagBlock.id != id)
     })
   }
 
+  const postImage = () => {
+    const formData = new FormData();
+    for (let i=0; i<reqImage.length; i++) {
+      formData.append('file', reqImage[i]);
+    }
+    
+    for (const key of formData.keys()) {
+      console.log(key);
+    }
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+    console.log(formData);
+    axios.post(`/api/feed/post/uploadImage`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then((res) => {
+      setInput({
+        ...input,
+        ["feedImageUrls"]: res.data.body
+      });
+      console.log("이미지 업로드 성공");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const LoginCheck = () => {
+    let token = sessionStorage.getItem('token');
+    if (token){
+      setInput({
+        ...input,
+        ["memberToken"]: sessionStorage.getItem("token")
+      });
+      console.log("로그인 확인")
+    } else {
+      alert("로그인을 해주세요")
+      navigate("/");
+    }
+  }
+
+  const postWriting = () => {
+    if(input["title"]){
+      postImage();
+      axios.post(`/api/feed/post`, input)
+      .then((res) => {
+        console.log(res);
+        alert("글이 정상적으로 등록되었습니다");
+        navigate("../pages/CommunityPage");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } else {
+      alert("글 제목은 필수입력입니다");
+    }
+    
+  };
+
+  useEffect(() => {
+    LoginCheck();
+  }, []);
+  
   return (
     <>
       <Style.Wrapper>
@@ -259,7 +380,7 @@ function Writing() {
         <Style.Article>
           <Style.Content>
             <Style.WriteTitleWrap>
-              <Style.WriteName>글 제목</Style.WriteName>
+              <Style.WriteName><Style.Span>*&nbsp;&nbsp;</Style.Span>글 제목</Style.WriteName>
               <Style.TitleInput
                 type="text"
                 name="title"
@@ -282,33 +403,63 @@ function Writing() {
             <Style.WriteImgWrap>
               <Style.WriteName>이미지&nbsp;</Style.WriteName>
               <Style.Imgs>
+                {previewImage[0]? 
+                <Style.WritingImg>
+                  <Style.Image src={previewImage[0]}></Style.Image>
+                  <Style.ImgDelBtn onClick={()=>deleteImg(previewImage[0])}><FontAwesomeIcon icon={faXmark} color="#C3C3C3"size="2x" /></Style.ImgDelBtn> 
+                </Style.WritingImg>
+                : 
                 <Style.InputImgWrap>
-                  <Style.InputImgBtn htmlFor="img1"><FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/></Style.InputImgBtn>
+                  <Style.InputImgBtn htmlFor="img1">
+                    <FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/>
+                  </Style.InputImgBtn>
                   <input
                     type="file"
                     accept="image/*"
                     id="img1"
+                    ref={img1Input}
                     style={{display: "none"}}
+                    onChange={(e) => onChangeImg(e, 0)}
                   />
-                </Style.InputImgWrap>
+                </Style.InputImgWrap>}
+                {previewImage[1]? 
+                <Style.WritingImg>
+                  <Style.Image src={previewImage[1]}></Style.Image>
+                  <Style.ImgDelBtn onClick={()=>deleteImg(previewImage[1])}><FontAwesomeIcon icon={faXmark} color="#C3C3C3"size="2x" /></Style.ImgDelBtn> 
+                </Style.WritingImg>
+                : 
                 <Style.InputImgWrap>
-                  <Style.InputImgBtn htmlFor="img2"><FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/></Style.InputImgBtn>
+                  <Style.InputImgBtn htmlFor="img2">
+                    <FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/>
+                  </Style.InputImgBtn>
                   <input
                     type="file"
                     accept="image/*"
                     id="img2"
+                    ref={img2Input}
                     style={{display: "none"}}
+                    onChange={(e) => onChangeImg(e, 1)}
                   />
-                </Style.InputImgWrap>
+                </Style.InputImgWrap>}
+                {previewImage[2]? 
+                <Style.WritingImg>
+                  <Style.Image src={previewImage[2]}></Style.Image>
+                  <Style.ImgDelBtn onClick={()=>deleteImg(previewImage[2])}><FontAwesomeIcon icon={faXmark} color="#C3C3C3"size="2x" /></Style.ImgDelBtn> 
+                </Style.WritingImg>
+                : 
                 <Style.InputImgWrap>
-                  <Style.InputImgBtn htmlFor="img3"><FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/></Style.InputImgBtn>
+                  <Style.InputImgBtn htmlFor="img3">
+                    <FontAwesomeIcon icon={faCamera} size="2x" color="#C3C3C3"/>
+                  </Style.InputImgBtn>
                   <input
                     type="file"
                     accept="image/*"
                     id="img3"
+                    ref={img3Input}
                     style={{display: "none"}}
+                    onChange={(e) => onChangeImg(e, 2)}
                   />
-                </Style.InputImgWrap>
+                </Style.InputImgWrap>}
               </Style.Imgs>
             </Style.WriteImgWrap>
 
@@ -330,14 +481,18 @@ function Writing() {
             <Style.TagWrap>
               <Style.WriteName>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Style.WriteName>
               <Style.Tags>
-                {tagBlocks}
+                {tagBlocks.map(tagBlock => {
+                  return (
+                    <>{tagBlock.value}</>
+                  )
+                })}
               </Style.Tags>
             </Style.TagWrap>  
           </Style.Content>
         </Style.Article>
 
         <Style.SubmitBtnWrap>
-          <Style.SubmitBtn>글쓰기</Style.SubmitBtn>
+          <Style.SubmitBtn onClick={postWriting}>글쓰기</Style.SubmitBtn>
         </Style.SubmitBtnWrap>
       </Style.Wrapper>
     </>
