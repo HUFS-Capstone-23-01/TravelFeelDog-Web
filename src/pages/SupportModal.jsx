de6f91a9d7938b3a7ae476af42e0b48cd980fe3d
Lim_JiSeon <dpf226@naver.com>
2023-06-05 18:02:11 +0900

    [FEAT] 지원 약관 작성 및 지원 약관 페이지 구현

--- src/pages/SupportModal.jsx
+++ src/pages/SupportModal.jsx
@@ -1,12 +1,25 @@
+import Footer from "../containers/Footer";
+import Header from "../containers/Header";
+import styled from "styled-components";
+import Support from "../modal/Support";
+
 const Style = {
-      
-}; 
+  Wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  `,
+};
 
 function SupportModal() { 
     
   return (
     <>
-      지원사항 모달창
+      <Style.Wrapper>
+        <Header></Header>
+        <Support></Support>
+        <Footer></Footer>
+      </Style.Wrapper>
     </>
   );
 }
